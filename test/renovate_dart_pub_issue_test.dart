c8211a10e1d132659111bad142317466f27335e9
KoheiKanagu <kanagu@kingu.dev>
2024-01-16 12:41:26 +0900

    fix

--- test/renovate_dart_pub_issue_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:renovate_dart_pub_issue/renovate_dart_pub_issue.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
