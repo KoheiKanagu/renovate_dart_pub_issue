03be6673822f90cfe887f592c4c90ef8cce72ef8
KoheiKanagu <kanagu@kingu.dev>
2024-01-16 12:38:09 +0900

    initial commit

---
+++ bin/renovate_dart_pub_issue.dart
@@ -0,0 +1,5 @@
+import 'package:renovate_dart_pub_issue/renovate_dart_pub_issue.dart' as renovate_dart_pub_issue;
+
+void main(List<String> arguments) {
+  print('Hello world: ${renovate_dart_pub_issue.calculate()}!');
+}
